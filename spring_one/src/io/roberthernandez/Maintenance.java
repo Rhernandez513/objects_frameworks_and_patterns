a03e5db2d42e6c841c378b94ba6ae63c433d20bf
Robert Hernandez <rhernandez513@gmail.com>
2018-02-13 18:52:47 -0600

    add getters and setters for maintenance objects

--- spring_one/src/io/roberthernandez/Maintenance.java
+++ spring_one/src/io/roberthernandez/Maintenance.java
@@ -5,4 +5,36 @@ public class Maintenance {
     private Facility facility;
     private Schedule schedule;
     private double cost;
+
+    public MaintenanceRequest getRequest() {
+        return request;
+    }
+
+    public void setRequest(MaintenanceRequest input) {
+        this.request = input;
+    }
+
+    public Facility getFacility() {
+        return this.facility;
+    }
+
+    public void setFacility(Facility input ) {
+        this.facility = input;
+    }
+
+    public Schedule getSchedule() {
+        return this.schedule;
+    }
+
+    public void setSchedule(Schedule input) {
+        this.schedule = input;
+    }
+
+    public double getCost() {
+        return this.cost;
+    }
+
+    public void setCost(double input) {
+        this.cost = input;
+    }
 }
--- spring_one/src/io/roberthernandez/MaintenanceRequest.java
+++ spring_one/src/io/roberthernandez/MaintenanceRequest.java
@@ -3,4 +3,20 @@ package io.roberthernandez;
 public class MaintenanceRequest {
     private Facility facility;
     private User user;
+
+    public void setFacility(Facility input) {
+        this.facility = input;
+    }
+
+    public Facility getFacility() {
+        return facility;
+    }
+
+    public void setUser(User input) {
+        this.user = input;
+    }
+
+    public User getUser() {
+        return user;
+    }
 }
