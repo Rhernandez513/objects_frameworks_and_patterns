414e9edb484d8f24bcdb11183df18ad621e543ce
Robert Hernandez <rhernandez513@gmail.com>
2018-02-13 18:59:55 -0600

    added some logic to the main

--- spring_one/src/io/roberthernandez/mainapp.java
+++ spring_one/src/io/roberthernandez/mainapp.java
@@ -1,8 +1,45 @@
 package io.roberthernandez;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class mainapp {
     public static void main(String[] args) {
 
+
+        System.out.println("Creating users and facilities, attempting to create a maintenance request on said facility");
+        User user_one = new User();
+        Facility facility_one = new Facility();
+        MaintenanceRequest request = new MaintenanceRequest();
+
+        request.setFacility(facility_one);
+        request.setUser(user_one);;
+
+        System.out.println("Now that the request is created we should be able to send it to the manangement system for scheduling");
+//        request.send();
+
+        System.out.println("In order to schedule we need a start and end time");
+
+        Schedule schedule = createSchedule();
+
+    }
+    public static Schedule createSchedule() {
+        Date first_time = new Date();
+
+        Schedule schedule = new Schedule();
+
+        schedule.setStartTime(first_time);
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        Date second_time = new Date();
+
+        schedule.setEndTime(second_time);
+
+        return schedule;
     }
 }
 
