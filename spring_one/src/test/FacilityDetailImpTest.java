26baa5387311cd8ac1d62b42cd7c72564d834bdb
Robert Hernandez <rhernandez513@gmail.com>
2018-04-09 13:05:05 -0500

    Add a handful of tests

---
+++ spring_one/src/test/FacilityDetailImpTest.java
@@ -0,0 +1,32 @@
+package test;
+
+import io.roberthernandez.Model.FacilManag.FacilityDetailImp;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class FacilityDetailImpTest {
+
+    @Test
+    void setDetailTest() {
+        FacilityDetailImp facilityDetailImp = new FacilityDetailImp();
+        facilityDetailImp.setDetail("Detail");
+        String result = facilityDetailImp.getDetail();
+        Assert.assertEquals(result, "Detail");
+    }
+
+    @Test
+    void getDetailTest() {
+        FacilityDetailImp facilityDetailImp = new FacilityDetailImp();
+        facilityDetailImp.setDetail("Detail");
+        String result = facilityDetailImp.getDetail();
+        Assert.assertEquals(result, "Detail");
+    }
+
+    @Test
+    void toStringTest() {
+        FacilityDetailImp facilityDetailImp = new FacilityDetailImp();
+        facilityDetailImp.setDetail("Detail");
+        String result = facilityDetailImp.toString();
+        Assert.assertEquals(result, "Detail");
+    }
+}
